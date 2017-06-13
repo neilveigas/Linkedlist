36e1541034ffd0845777d86497bf136236160e39
neilveigas <neilveigas@gmail.com>
2017-06-12 22:16:46 -0400

    added file

---
+++ LinkedList.java
@@ -0,0 +1,65 @@
+import java.util.Iterator;
+public class LinkedList implements Iterable {
+        private Node head;
+        private Node tail;
+        private int size;
+     
+        private class Node {
+            String value;
+            Node link;
+     
+            Node(String value) {
+                this.value = value;
+                size++;
+            }
+        }
+     
+        public LinkedList() {
+            head = tail = null;
+            size = 0;
+        }
+     
+        public void add(String s) {
+            Node n = new Node(s);
+            if (head == null)
+                head = n;
+            if (tail != null)
+                tail.link = n;
+            tail = n;
+        }
+        public int getSize() {
+            return size;
+        }
+     
+        public String[] toArray() {
+            String[] a = new String[size];
+            int i = 0;
+            Node n = head;
+            while (n != null) {
+                a[i++] = n.value;
+                n = n.link;
+            }
+            return a;
+        }
+        
+        public Iterator iterator() {
+        return new Iterator() {
+            Node current = head;
+ 
+            public boolean hasNext() {
+                return current != null;
+            }
+ 
+            public String next() {
+                Node t = current;
+                current = current.link;
+                return t.value;
+            }
+ 
+            public void remove() {
+            }
+        };
+    }
+    }
+
+
